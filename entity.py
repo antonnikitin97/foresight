b8b3716d01f3bc7cdd909547b0ca0525b0ac2969
Charlie <cst1g16@soton.ac.uk>
2018-02-04 18:29:13 +0000

    Populate entity methods with initial version of code

--- entity.py
+++ entity.py
@@ -1,9 +1,19 @@
+import pygame
+from move import Move
+
 class Entity:
-    def __init__(self):
-        pass
+    def __init__(self, x, y, spritelist, spriteindex=0):
+        self.x = x
+        self.y = y
+        self.moves = []
+        self.spritelist = spritelist
+        self.spriteindex = spriteindex
+        self.sprite = spritelist[spriteindex]
 
-    def move(self):
-        pass
+    def move(self, diffx, diffy, backlog=1):
+        self.moves.append(Move(diffx, diffy, self))
+        if len(self.moves) > backlog:
+            self.moves.pop(0).move()
 
-    def show(self):
-        pass
+    def show(self, screen):
+        screen.blit(screen, (self.x, self.y))
